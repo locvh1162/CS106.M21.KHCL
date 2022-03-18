66499558474bf7dbcc8a088b88955ba7eb30747d
Vo Hoang Loc <locvh1162@gmail.com>
2022-03-18 09:40:20 +0700

    Add BFS code

--- Assignments/Assignment 1 - DFS-BFS-UCS for Sokoban/sokoban/solver.py
+++ Assignments/Assignment 1 - DFS-BFS-UCS for Sokoban/sokoban/solver.py
@@ -180,7 +180,20 @@ def breadthFirstSearch(gameState):
     exploredSet = set()
     temp = []
     ### Implement breadthFirstSearch here
-    
+    while frontier:
+        node = frontier.popleft()
+        node_action = actions.popleft()
+        if isEndState(node[-1][-1]):
+            temp += node_action[1:]
+            break
+        if node[-1] not in exploredSet:
+            exploredSet.add(node[-1])
+            for action in legalActions(node[-1][0], node[-1][1]):
+                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                if isFailed(newPosBox):
+                    continue
+                frontier.append(node + [(newPosPlayer, newPosBox)])
+                actions.append(node_action + [action[-1]])
     return temp
     
 def cost(actions):
